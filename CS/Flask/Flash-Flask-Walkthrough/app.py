753648006d8765f1a81cf49720bcda8995acb56f
Isaiah997 <isaiahalexander.ames@hgs.hiddengeniusproject.org>
2025-05-01 17:33:28 -0700

    Update app.py
    Signed-off-by: Isaiah997 <isaiahalexander.ames@hgs.hiddengeniusproject.org>

--- CS/Flask/Flash-Flask-Walkthrough/app.py
+++ CS/Flask/Flash-Flask-Walkthrough/app.py
@@ -1,16 +1,11 @@
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    user_name = "Alice"
+    return render_template('home.html', name=user_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #new changes 
